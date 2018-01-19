f11032d4ccfa8b27eb01b31ef9f76991d9e8a953
Simon Hoyos <shmesa23@gmail.com>
2018-01-19 13:35:51 -0500

    Refactor: destructuring props object

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Button.css';
 
-const Button = (props) => {
-  const bgColor = props.primary ? '#4682b4' : '#C0625E';
+const Button = ({ primary, className, children }) => {
+  const bgColor = primary ? '#4682b4' : '#C0625E';
 
   return (
     <button
-      className={`add-button ${props.className}`}
+      className={`add-button ${className}`}
       type='button'
       style={{ backgroundColor: bgColor }}
     >
-      {props.children}
+      {children}
     </button>
   );
 }
